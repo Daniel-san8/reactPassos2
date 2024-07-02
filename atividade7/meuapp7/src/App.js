d6f234c6a2de7b4ca65b3bbefb5fd7e61e826bc9
Daniel San <danielgdt88@hotmail.com>
2024-07-01 22:07:37 -0300

    useRef

--- atividade7/meuapp7/src/App.js
+++ atividade7/meuapp7/src/App.js
@@ -1,31 +1,24 @@
 import React from "react";
 
 function App() {
-  const [comentarios, setComentarios] = React.useState([]);
-  const inputElement = React.useRef();
-  const [input, setInput] = React.useState("");
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notificacao, setNotificacao] = React.useState(null);
+  const memoria = React.useRef();
 
   function handleClick() {
-    setComentarios([...comentarios, input]);
-    setInput("");
-    inputElement.current.focus();
+    setCarrinho(carrinho + 1);
+    setNotificacao("Item adicionado ao carrinho");
+    clearTimeout(memoria.current);
+
+    memoria.current = setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
   }
 
   return (
     <div>
-      <ul>
-        {comentarios.map((comentario) => {
-          return <li key={comentario}>{comentario}</li>;
-        })}
-      </ul>
-
-      <input
-        ref={inputElement}
-        type="text"
-        value={input}
-        onChange={({ target }) => setInput(target.value)}
-      />
-      <button onClick={handleClick}>Enviar</button>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Total {carrinho}</button>
     </div>
   );
 }
