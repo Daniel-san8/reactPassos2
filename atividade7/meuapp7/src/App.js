bfd330d3abddd03bf2709d371c431f728efd1b79
Daniel San <danielgdt88@hotmail.com>
2024-07-02 16:20:41 -0300

    aprendendo user context

--- atividade7/meuapp7/src/App.js
+++ atividade7/meuapp7/src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
 
 function App() {
-  const [contar, setContar] = React.useState(0);
-  const handleClick = React.useCallback(() => {
-    setContar((contar) => contar + 1);
-  }, []);
-  return (
-    <div>
-      <button onClick={handleClick}>Total {contar}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ atividade7/meuapp7/src/GlobalContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalContext = () => {
+  const userContext = React.useContext();
+};
+
+export default GlobalContext;
