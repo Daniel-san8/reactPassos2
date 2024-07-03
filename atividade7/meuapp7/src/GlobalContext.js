1f47c2c7dc04d9f84532ef6c724e316a46fdf64c
Daniel San <danielgdt88@hotmail.com>
2024-07-03 19:40:13 -0300

    iniciando function de requisição

--- atividade7/meuapp7/src/GlobalContext.js
+++ atividade7/meuapp7/src/GlobalContext.js
@@ -3,8 +3,25 @@ import React from "react";
 export const GlobalContext = React.createContext();
 
 export const GlobalDadosApi = ({ children }) => {
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    const requisition = async () => {
+      try {
+        const requisitionApi = await fetch(
+          "https://ranekapi.origamid.dev/json/api/produto/"
+        );
+        const requisitionApiJson = await requisitionApi.json();
+        setDados((dados) => (dados = requisitionApiJson));
+      } catch {
+        console.log("err");
+      }
+      requisition();
+    };
+  }, []);
+
   return (
-    <GlobalContext.Provider value={{ daniel: "daniel" }}>
+    <GlobalContext.Provider value={{ daniel: "daniel", dados }}>
       {children}
     </GlobalContext.Provider>
   );
--- atividade7/meuapp7/src/Produto.js
+++ atividade7/meuapp7/src/Produto.js
@@ -3,6 +3,7 @@ import { GlobalContext } from "./GlobalContext";
 
 const Produto = () => {
   const globalDados = React.useContext(GlobalContext);
+  console.log(globalDados.dados);
   return <div>{globalDados.daniel}</div>;
 };
 
