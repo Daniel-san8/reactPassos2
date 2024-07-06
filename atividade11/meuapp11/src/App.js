74c9116911a658d97395ee323763fc455dc5e6fb
Daniel San <danielgdt88@hotmail.com>
2024-07-05 21:40:40 -0300

    radio e select com react

--- atividade11/meuapp11/src/App.js
+++ atividade11/meuapp11/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Input from "./Form/Input";
 import Select from "./Form/Select";
+import Radio from "./Form/Radio";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("");
+  const [frutas, setFrutas] = React.useState("");
 
   return (
     <form>
@@ -22,6 +25,14 @@ const App = () => {
         value={produto}
         setValue={setProduto}
       />
+      <Radio options={["Azul", "Vermelho"]} value={cor} setValue={setCor} />
+      <h2>Frutas:</h2>
+      <Radio
+        options={["Limão", "Goiaba", "Uva"]}
+        value={frutas}
+        setValue={setFrutas}
+      />
+
       <button>Enviar</button>
     </form>
   );
---
+++ atividade11/meuapp11/src/Form/Radio.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Radio = ({ options, value, setValue, ...props }) => {
+  return (
+    <React.Fragment>
+      {options.map((option) => {
+        return (
+          <label key={option}>
+            <input
+              type="radio"
+              value={option}
+              checked={value === option}
+              onChange={({ target }) => setValue(target.value)}
+              {...props}
+            />
+            {option}
+          </label>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+export default Radio;
