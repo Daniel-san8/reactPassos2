e7e109222c9370db2dbc231b64f5929eb7f2321b
Daniel San <danielgdt88@hotmail.com>
2024-07-08 11:27:54 -0300

    primeiros aprendizados

--- next/src/app/sobre/page.tsx
+++ next/src/app/sobre/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sobre",
+  description: "Esta é a página sobre",
+};
+
 export default function SobrePage() {
   return (
     <main>
