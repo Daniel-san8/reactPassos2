f8f1080639f10b4e1baa7353476699a307ba83f9
Daniel San <danielgdt88@hotmail.com>
2024-07-09 23:38:30 -0300

    começando com ts

--- typescript/typescript1.ts
+++ typescript/typescript1.ts
@@ -1,7 +1,14 @@
 const frase: string = "Front End";
 const total = 100.05;
+const empresas: string[] = ["Apple", "Microsoft"];
 
 // total.toLowerCase();
 frase.toLowerCase();
 
 total.toFixed(2);
+
+empresas.map((empresa) => empresa.toLowerCase());
+
+const body = document.body;
+
+body.style.background = "black";
