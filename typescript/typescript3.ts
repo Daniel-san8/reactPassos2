fead90b62fc8e88c7f6fc86a1a6daf2531b86a38
Daniel San <danielgdt88@hotmail.com>
2024-07-11 09:25:17 -0300

    aprendendo union e types

--- typescript/typescript3.ts
+++ typescript/typescript3.ts
@@ -1,3 +1,13 @@
 function somar(a: number, b: number) {
   return a + b;
 }
+
+somar(5, 10);
+
+let produto: string = "Livro";
+let preco: number = 200;
+
+const carro = {
+  marca: "Audi",
+  portas: 5,
+};
---
+++ typescript/typescript4.ts
@@ -0,0 +1,7 @@
+let preco = 200;
+
+const barato = preco > 400 ? true : "Caro";
+
+function somar(a: number, b: number) {
+  return a + b;
+}
