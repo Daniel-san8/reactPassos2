ee1d26b117bfa3d0f0e8ebee8346abe37012bfb6
Daniel San <danielgdt88@hotmail.com>
2024-07-13 18:55:34 -0300

    undefined

--- typescript/nullEundefined.ts
+++ typescript/nullEundefined.ts
@@ -3,4 +3,14 @@ const config = localStorage.getItem("config");
 
 if (button !== null) true;
 
+if (button) {
+  button.click();
+}
+
 button?.click();
+
+let totall;
+
+if (totall) {
+  console.log(`^${totall} foi definido`);
+}
