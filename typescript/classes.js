8c9d80d860a1b992e73f9fd8445a5e1249d19ed0
Daniel San <danielgdt88@hotmail.com>
2024-07-13 19:26:53 -0300

    extends

--- typescript/classes.js
+++ typescript/classes.js
@@ -13,3 +13,26 @@ var livroo = new Produtoo("Fundamentos Apostólicos", 200);
 livroo.name;
 livroo.preco;
 livroo.precoReal();
+console.log(livroo instanceof Produtoo);
+var Livro = /** @class */ (function () {
+    function Livro(autor) {
+        this.autor = autor;
+    }
+    return Livro;
+}());
+var Jogadores = /** @class */ (function () {
+    function Jogadores(jogadores) {
+        this.jogadores = jogadores;
+    }
+    return Jogadores;
+}());
+function buscarProduto(busca) {
+    if (busca === "O Hobbit") {
+        return new Livro("J. R. R Tolkien");
+    }
+    if (busca === "Dark Souls") {
+        return new Jogadores(1);
+    }
+    return null;
+}
+buscarProduto("O Hobbit");
--- typescript/classes.ts
+++ typescript/classes.ts
@@ -1,17 +1,59 @@
 class Produtoo {
   name: string;
-  preco: number;
-  constructor(name: string, preco: number) {
+  constructor(name: string) {
     this.name = name;
-    this.preco = preco;
   }
-  precoReal = () => {
-    return `R$: ${this.preco}`;
-  };
 }
 
-const livroo = new Produtoo("Fundamentos Apostólicos", 200);
+const livroo = new Produtoo("Fundamentos Apostólicos");
 
 livroo.name;
-livroo.preco;
-livroo.precoReal();
+
+console.log(livroo instanceof Produtoo);
+
+class Livro extends Produtoo {
+  autor: string;
+  constructor(name: string, autor: string) {
+    super(name);
+    this.autor = autor;
+  }
+}
+
+class Jogadores {
+  jogadores: number;
+  constructor(jogadores: number) {
+    this.jogadores = jogadores;
+  }
+}
+
+function buscarProduto(busca: string) {
+  if (busca === "O Hobbit") {
+    return new Livro("J. R. R Tolkien", "O Hobbit");
+  }
+
+  if (busca === "Dark Souls") {
+    return new Jogadores(1);
+  }
+
+  return null;
+}
+
+const produtao = buscarProduto("O Hobbit");
+
+if (produtao instanceof Jogadores) {
+  produtao.jogadores;
+}
+
+if (produtao instanceof Produtoo) {
+  console.log(produtao.name);
+}
+
+interface Carro {
+  nome: string;
+}
+
+// NÃO FUNCIONA COM INSTANCEOF
+
+const honda: Carro = {
+  nome: "Honda",
+};
