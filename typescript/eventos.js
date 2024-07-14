ed29c599899e5e789e34a364f89db2549dd921f8
Daniel San <danielgdt88@hotmail.com>
2024-07-14 18:14:13 -0300

    eventos

---
+++ typescript/eventos.js
@@ -0,0 +1,5 @@
+var botton = document.querySelector("button");
+function handleClick(event) {
+    console.log(event);
+}
+botton === null || botton === void 0 ? void 0 : botton.addEventListener("click", handleClick);
---
+++ typescript/eventos.ts
@@ -0,0 +1,25 @@
+const botton = document.querySelector("button");
+
+function handleClick(event: PointerEvent) {
+  console.log(event);
+}
+
+botton?.addEventListener("pointerdown", handleClick);
+
+function handleScroll(evento: Event) {
+  console.log(evento);
+}
+
+window.addEventListener("scroll", handleScroll);
+
+function ativarMenu(event: Event) {
+  if (event instanceof MouseEvent) {
+    console.log(event.pageX);
+  } else if (event instanceof TouchEvent) {
+    console.log(event.touches[0].pageX);
+  }
+}
+
+document.documentElement.addEventListener("mousedown", ativarMenu);
+document.documentElement.addEventListener("touchstart", ativarMenu);
+document.documentElement.addEventListener("keydown", ativarMenu);
