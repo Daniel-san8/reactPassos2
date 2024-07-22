edddf663c741c4806d6a32bea7b4f0c2ef4db826
Daniel San <danielgdt88@hotmail.com>
2024-07-22 20:41:10 -0300

    aprendendo routes

--- atividade12/meuapp12/src/App.js
+++ atividade12/meuapp12/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
