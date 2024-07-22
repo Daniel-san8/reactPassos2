d0d6447b8a940467f7f89ba4b0de01afd62b2899
Daniel San <danielgdt88@hotmail.com>
2024-07-22 20:53:53 -0300

    aprendendo routes

--- atividade12/meuapp12/src/App.js
+++ atividade12/meuapp12/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Sobre from "./Sobre";
+import NaoEncontrada from "./NaoEncontrada";
 
 function App() {
   return (
     <BrowserRouter>
+      <h1>Esse é o header</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
     </BrowserRouter>
   );
