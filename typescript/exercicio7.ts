cd1c4cbd1283333105f2ec41db407ffa1c636663
Daniel San <danielgdt88@hotmail.com>
2024-08-05 22:20:47 -0300

    exercicio 7

--- typescript/exercicio7.ts
+++ typescript/exercicio7.ts
@@ -1,11 +1,4 @@
-async function consumirApi() {
-  const chama = await fetch("https://api.origamid.dev/json/cursos.json");
-  const json = await chama.json();
-  console.log(json);
-}
-consumirApi();
-
-interface api {
+interface Api {
   aulas: number;
   gratuito: boolean;
   horas: number;
@@ -14,3 +7,19 @@ interface api {
   nome: string;
   tags: string[];
 }
+
+async function consumirApi() {
+  const chama = await fetch("https://api.origamid.dev/json/cursos.json");
+  const json = await chama.json();
+  if (json)
+    json.map((item: Api) => {
+      if (item.nome && item.horas && item.tags) {
+        document.body.innerHTML += `
+            <p>${item.nome}</p>
+            <p>${item.horas}</p>
+            <p>${item.tags.map((item) => item)}</p>
+        `;
+      }
+    });
+}
+consumirApi();
