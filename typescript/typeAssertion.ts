7d798817ea412f84a9f7688f7c8cfb9cd7f962bc
Daniel San <danielgdt88@hotmail.com>
2024-08-06 10:28:20 -0300

    type assertion

---
+++ typescript/typeAssertion.ts
@@ -0,0 +1,7 @@
+const video = document.querySelector(".player") as HTMLVideoElement;
+
+video.volume;
+
+// const opa = document.querySelector(".teste")!;
+
+const opa = <HTMLVideoElement>document.querySelector(".teste");
