fe10c353cc3fb546e088951070acd0dd177a9474
Daniel San <danielgdt88@hotmail.com>
2024-08-17 20:44:02 -0300

    criada estrutura do slide

--- atividade13/src/App.jsx
+++ atividade13/src/App.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import Slide from "./Slide";
 
 function App() {
-  return <></>;
+  const slides = [
+    {
+      id: "slide1",
+      text: "Slide 1",
+    },
+    {
+      id: "slide2",
+      text: "Slide 2",
+    },
+    {
+      id: "slide3",
+      text: "Slide 3",
+    },
+  ];
+
+  return <Slide slides={slides} />;
 }
 
 export default App;
