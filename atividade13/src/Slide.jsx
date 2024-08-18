e3cb0456a9f48062bd1c4f1983dac388e25b1468
Daniel San <danielgdt88@hotmail.com>
2024-08-17 21:00:35 -0300

    animation completa

--- atividade13/src/Slide.jsx
+++ atividade13/src/Slide.jsx
@@ -2,16 +2,41 @@ import React from "react";
 import styles from "./Slide.module.css";
 
 const Slide = ({ slides }) => {
+  const [active, setActive] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
+  const contentRef = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = contentRef.current.getBoundingClientRect();
+    setPosition(-(width * active));
+  }, [active]);
+
+  function slidePrev() {
+    if (active > 0) setActive(active - 1);
+  }
+
+  function slideNext() {
+    if (active < slides.length - 1) setActive(active + 1);
+  }
+
   return (
     <section className={styles.container}>
-      <div className={styles.content}>
+      <div
+        className={styles.content}
+        style={{ transform: `translateX(${position}px)` }}
+        ref={contentRef}
+      >
         {slides.map((slide) => {
-          return <div key={slide.id}>{slide.text}</div>;
+          return (
+            <div key={slide.id} className={styles.item}>
+              {slide.text}
+            </div>
+          );
         })}
       </div>
       <nav className={styles.nav}>
-        <button>Anterior</button>
-        <button>Próximo</button>
+        <button onClick={slidePrev}>Anterior</button>
+        <button onClick={slideNext}>Próximo</button>
       </nav>
     </section>
   );
