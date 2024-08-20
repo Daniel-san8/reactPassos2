6914df65dbda0d5d1c3f894400fe44f3940ec78d
Daniel San <danielgdt88@hotmail.com>
2024-08-20 17:52:26 -0300

    prop types

---
+++ atividade16/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <>
+      <Button width={100}>Clique Aqui</Button>
+    </>
+  );
+};
+
+export default App;
