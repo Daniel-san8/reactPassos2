e1a8e85764ad4855bfa511b421d5b1f719194ea5
Daniel San <danielgdt88@hotmail.com>
2024-08-21 10:41:44 -0300

    props

--- reactTypescript/primeiroArquivo/src/App.tsx
+++ reactTypescript/primeiroArquivo/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Button from "./Button";
 
 function App() {
-  return <div>React</div>;
+  const [total, setTotal] = React.useState(0);
+
+  function incrementar() {
+    setTotal((total) => total + 1);
+  }
+  return (
+    <>
+      <button onClick={incrementar}>{total}</button>
+      {/* <Button tamanho="3rem" /> */}
+      <Button onClick={incrementar}>Incrementar 2</Button>
+    </>
+  );
 }
 
 export default App;
---
+++ reactTypescript/primeiroArquivo/src/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+type ButtonProps = {
+  tamanho?: string;
+  children?: ReactNode;
+  onClick?: () => void;
+};
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button onClick={props.onClick} style={{ fontSize: props.tamanho }}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
