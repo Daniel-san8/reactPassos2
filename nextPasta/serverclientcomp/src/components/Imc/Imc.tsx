45bd1f606f23f573509b882e5c0d7b30e0bc6915
Daniel San <danielgdt88@hotmail.com>
2024-09-17 18:07:03 -0300

    feat: exercicio feito

--- nextPasta/serverclientcomp/src/components/Imc/Imc.tsx
+++ nextPasta/serverclientcomp/src/components/Imc/Imc.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import React from "react";
+
 const Imc = () => {
+  const altura = React.useRef<HTMLInputElement | null>(null);
+  const peso = React.useRef<HTMLInputElement | null>(null);
+  const [resultado, setResultado] = React.useState(0);
+
+  function calculaImc() {
+    if (altura.current && peso.current) {
+      const calculoImc =
+        +peso.current.value /
+        (parseFloat(altura.current.value) * parseFloat(altura.current.value));
+      setResultado(calculoImc);
+    }
+  }
+
   return (
     <div>
-      <h1>Calculo de IMC</h1>
+      <h1>Calculo de IMC:</h1>
+      <label htmlFor="peso">Peso:</label>
+      <input type="number" id="peso" ref={peso} />
+      <div>
+        <label htmlFor="altura">Altura:</label>
+        <input type="text" id="altura" ref={altura} />
+      </div>
+      {resultado ? <p>Resultado: {resultado}</p> : null}
+
+      <button onClick={calculaImc}>Calcular</button>
     </div>
   );
 };
