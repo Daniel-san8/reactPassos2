69fa2602c66200cb8364d03a1f046398dbf445df
Daniel San <danielgdt88@hotmail.com>
2024-09-17 16:33:11 -0300

    Server e Client Componentes

---
+++ nextPasta/serverclientcomp/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Server e Client Componentes",
+  description: "Server e Client Componentes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
