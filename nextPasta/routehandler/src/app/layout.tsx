f48b1098ffb6758517dfd277fb896955a1fb467b
Daniel San <danielgdt88@hotmail.com>
2024-09-23 20:31:11 -0300

    arquivos route handler

---
+++ nextPasta/routehandler/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
