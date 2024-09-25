8553cfa904d90ad41ad3641c73a1dfe94f415329
Daniel San <danielgdt88@hotmail.com>
2024-09-25 11:20:53 -0300

    exerc feito

---
+++ nextPasta/exercroutehandler/src/app/api/login/route.ts
@@ -0,0 +1,27 @@
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = (await request.json()) as { username: string; senha: string };
+
+  const response = await fetch("https://api.origamid.online/conta/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(body),
+  });
+  console.log(response.ok);
+  if (!response.ok) {
+    return Response.json({ erro: "Erro na api" });
+  }
+
+  const json = await response.json();
+
+  cookies().set("cookie", json.token, {
+    httpOnly: true,
+    secure: true,
+  });
+  return Response.json(json);
+}
---
+++ nextPasta/exercroutehandler/src/app/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { FormEvent } from "react";
+
+export default function Home() {
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const username = event.currentTarget.login.value;
+    const senha = event.currentTarget.senha.value;
+
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({ username, senha }),
+    });
+
+    console.log(response);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="login">Login:</label>
+        <input type="text" id="login" />
+
+        <br />
+
+        <label htmlFor="senha">Senha:</label>
+        <input
+          type="password"
+          id="senha"
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        />
+
+        <br />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  );
+}
