2d71ceb772464e0dd28b85adb0b1e3a25f59c1bd
Daniel San <danielgdt88@hotmail.com>
2024-09-25 22:10:33 -0300

    exerc feito

--- nextPasta/exercroutehandler/src/actions/set-cookie.tsx
+++ nextPasta/exercroutehandler/src/actions/set-cookie.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath, revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 
 export async function setCookie(key: string, value: string) {
@@ -7,4 +8,10 @@ export async function setCookie(key: string, value: string) {
     httpOnly: true,
     secure: true,
   });
+
+  // para revalidar o cache de uma rota. Funciona em apenas server components
+  //   revalidatePath("/api");
+
+  // para revalidar todas rodas a partir da rota do argumento, apenas em server components
+  //   revalidateTag("api");
 }
